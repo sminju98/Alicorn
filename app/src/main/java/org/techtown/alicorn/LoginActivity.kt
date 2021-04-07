465b84d58f5aa633b976a3ef5a2b402f10f11c02
sminju98 <sminju98@gmail.com>
2021-04-08 02:24:01 +0900

    구글 로그인

--- app/src/main/java/org/techtown/alicorn/LoginActivity.kt
+++ app/src/main/java/org/techtown/alicorn/LoginActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import org.techtown.alicorn.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     var auth: FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient?= null
+    var GOOGLE_LOGIN_CODE = 9001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -22,6 +30,47 @@ class LoginActivity : AppCompatActivity() {
         binding.emailSignupButton.setOnClickListener {
             signupEmail()
         }
+        binding.googleLoginButton.setOnClickListener {
+            //첫번째 단계
+            googleLogin()
+        }
+
+        var gso= GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("593874221588-7mqldmalbs82t54kd1pdoaga6e7g4quv.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess){
+                    var account = result.signInAccount
+                    //두번째 단계
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    //로그인 성공
+                    moveMainpage(task.result?.user)
+                } else {
+                    //로그인 실패, 에러메세지
+                    Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 
     fun signupEmail() {
@@ -59,7 +108,6 @@ class LoginActivity : AppCompatActivity() {
             }
 
     }
-
 
     fun moveMainpage(user: FirebaseUser?) {
         if (user != null) {
