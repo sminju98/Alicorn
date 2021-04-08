04eb2332ac5ad72330b6ae3d04340fbfef30314f
sminju98 <sminju98@gmail.com>
2021-04-08 14:53:35 +0900

    컨텐츠 데이터 모델

--- app/src/main/java/org/techtown/alicorn/navigation/AddPhotoActivity.kt
+++ app/src/main/java/org/techtown/alicorn/navigation/AddPhotoActivity.kt
@@ -6,9 +6,12 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import org.techtown.alicorn.R
 import org.techtown.alicorn.databinding.ActivityAddPhotoBinding
+import org.techtown.alicorn.navigation.model.ContentDTO
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -17,14 +20,19 @@ class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM =0
     var storage : FirebaseStorage? = null
     var photoUri : Uri? = null
+    var auth: FirebaseAuth? = null
+    var firestore : FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityAddPhotoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Initiate storage
+        //Initiate
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         //앨범 열기
         var photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -50,7 +58,7 @@ class AddPhotoActivity : AppCompatActivity() {
             }
     }
 
-    fun contentUpload(){
+    fun contentUpload() {
         //파일명 만들기
 
         var timestamp = SimpleDateFormat("yyyyMmdd_HHmmss").format(Date())
@@ -58,9 +66,34 @@ class AddPhotoActivity : AppCompatActivity() {
 
         var storageRef = storage?.reference?.child("images")?.child(imageFileName)
 
-        //파일 업로드
-        storageRef?. putFile(photoUri!!)?.addOnSuccessListener {
-            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        //promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask {
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnCompleteListener { uri ->
+            var contentDTO = ContentDTO()
+
+            //이미지 다운로드 주소 삽입
+            contentDTO.imageUrl = uri.toString()
+
+            //유저 uid 삽입
+            contentDTO.uid = auth?.currentUser?.uid
+
+            //userId 삽입
+            contentDTO.userId = auth?.currentUser?.email
+
+            //내용 설명 삽입
+            contentDTO.explain = binding.addphotoEditExplain.text.toString()
+
+            //timestamp 삽입
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            Toast.makeText(this, "사진 업로드 성공", Toast.LENGTH_LONG).show()
+
+            finish()
         }
     }
 }
