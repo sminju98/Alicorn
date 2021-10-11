1bad440725d9293889d6bfb9de23cb3a252600be
sminju98 <sminju98@gmail.com>
2021-10-11 22:20:05 +0900

    진료신청 레이아웃 작성

--- app/src/main/java/org/techtown/alicorn/DoctorActivity.kt
+++ app/src/main/java/org/techtown/alicorn/DoctorActivity.kt
@@ -2,7 +2,6 @@ package org.techtown.alicorn
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.firestore.ktx.firestore
@@ -20,8 +19,6 @@ class DoctorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDoctorBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        Toast.makeText(this,doctorId,Toast.LENGTH_SHORT).show()
 
         db.collection("doctor").document(doctorId).get().addOnSuccessListener{
             doctor = it.toObject(DoctorDTO::class.java)
